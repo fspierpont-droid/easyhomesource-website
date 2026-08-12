21a180e4b8cb7610db2ed0b3c16f2d35bf5e1d14
Tinyuniverse <f.s.pierpont@gmail.com>
2026-08-12 14:28:19 -0400

    Add files via upload

---
+++ data/boujee2ManufacturerMedia.ts
@@ -0,0 +1,135 @@
+import type { HomeMediaManifest } from "@/data/homeMedia";
+
+const sourcePage = "https://www.manufacturedhomes.com/home/226859/clayton-addison/addison/the-boujee/the-boujee-2-44bou28603bh/";
+
+export const boujee2ManufacturerMedia: HomeMediaManifest = {
+  "boujee-2": {
+    slug: "boujee-2",
+    gallery: [
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-01.jpg",
+        alt: "Boujee 2 44BOU28603BH floor plan",
+        category: "floorplan",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-02.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 1",
+        category: "interior",
+        isPrimary: true,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-03.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 2",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-04.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 3",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-05.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 4",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-06.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 5",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-07.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 6",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-08.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 7",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-09.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 8",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-10.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 9",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-11.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 10",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-12.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 11",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-13.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 12",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-14.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 13",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-15.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 14",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-16.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 15",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      },
+      {
+        src: "/homes/boujee-2/floorplan/boujee-2-floorplan-17.jpg",
+        alt: "Boujee 2 44BOU28603BH interior view 16",
+        category: "interior",
+        isPrimary: false,
+        sourceUrl: sourcePage
+      }
+    ],
+    floorPlanImage: "/homes/boujee-2/floorplan/boujee-2-floorplan-01.jpg",
+    brochureUrl: null,
+    videoUrl: null,
+    virtualTourUrl: null,
+    sourcePage
+  }
+};
